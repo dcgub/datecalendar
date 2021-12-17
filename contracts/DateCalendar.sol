8f081c4e30afc86a189ae5761f2f82bd40bf998e
Jeremie Holdom <jeremieholdom@gmail.com>
2021-12-17 12:04:09 -0500

    fixed name

--- contracts/DateCalendar.sol
+++ contracts/DateCalendar.sol
@@ -5,16 +5,16 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 /**
- * @title DatesCalendar contract
+ * @title DateCalendar contract
  * 
  * @dev Extends ERC721 Non-Fungible Token Standard basic implementation.
  */
-contract DatesCalendar is ERC721 {
+contract DateCalendar is ERC721 {
 
 
     /**
      * @dev Initialize the contract with the default `name` and `symbol`.
      */
-    constructor() public ERC721("DatesCalendar", "DC") {}
+    constructor() public ERC721("DateCalendar", "DC") {}
 
 }
