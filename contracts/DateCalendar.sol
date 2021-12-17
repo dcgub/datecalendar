8f6f06fd150aebacc41665352026ad97bc302db8
Jeremie Holdom <jeremieholdom@gmail.com>
2021-12-17 15:24:26 -0500

    env setup

--- contracts/DateCalendar.sol
+++ contracts/DateCalendar.sol
@@ -11,10 +11,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
  */
 contract DateCalendar is ERC721 {
 
+	// Unix epoch date (1970-01-01) index
+	uint256 private constant _unixEpochDateIndex = 36500;
 
     /**
      * @dev Initialize the contract with the default `name` and `symbol`.
      */
     constructor() public ERC721("DateCalendar", "DC") {}
+
+    /**
+     * @dev Mint a date calendar token.
+     */
+    function mintDate(uint256 dateIndex) public {
+
+    	_safeMint(msg.sender, dateIndex);
+
+    }
 
 }
