b619c130393d852442795b2ede8ca6f04c2128a0
Jeremie Holdom <jeremieholdom@gmail.com>
2021-12-18 00:27:50 -0500

    basic tests finished

--- contracts/DateCalendar.sol
+++ contracts/DateCalendar.sol
@@ -11,20 +11,45 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
  */
 contract DateCalendar is ERC721 {
 
-	// Unix epoch date (1970-01-01) index
-	uint256 private constant _unixEpochDateIndex = 36500;
+    // Unix epoch date (1970-01-01) index
+    uint256 private constant _unixEpochDateIndex = 36500;
 
     /**
      * @dev Initialize the contract with the default `name` and `symbol`.
      */
     constructor() public ERC721("DateCalendar", "DC") {}
 
+
+    /**
+     * @dev Returns the number of days since the Unix epoch (1970-01-01).
+     */
+    function _daysFromUnixEpoch() private view returns (uint256) {
+        return block.timestamp / 1 days;
+    }
+
+    /**
+     * @dev Determines the `dateIndex` given the current block.
+     */
+     function blockDateIndex() public view returns (uint256) {
+        return _unixEpochDateIndex + _daysFromUnixEpoch() - 1;
+     }
+
+
+    /**
+     * @dev Determines whether a date index has been released.
+     */
+     function _isReleased(uint256 dateIndex) private view returns (bool) {
+        return dateIndex <= blockDateIndex();
+     }
+
+
     /**
      * @dev Mint a date calendar token.
      */
     function mintDate(uint256 dateIndex) public {
+        require(_isReleased(dateIndex), "DateCalendar: date has not yet been released.");
 
-    	_safeMint(msg.sender, dateIndex);
+        _safeMint(msg.sender, dateIndex);
 
     }
 
---
+++ tests/test_datecalendar.py
@@ -0,0 +1,67 @@
+import brownie
+
+import pytest
+
+@pytest.fixture(scope="module", autouse=True)
+def dc(DateCalendar, accounts):
+    owner = accounts[0]
+    t = owner.deploy(DateCalendar)
+    yield t
+
+@pytest.fixture(autouse=True)
+def isolation(fn_isolation):
+    pass
+
+def test_mint(dc, accounts, chain):
+    acc = accounts[1]
+    # First mint
+    dc.mintDate(0, {'from': acc})
+
+    # Cannot be minted twice
+    with brownie.reverts():
+        dc.mintDate(0, {'from': acc})
+
+    # Can mint todoy's date
+    today_idx = dc.blockDateIndex()
+    dc.mintDate(today_idx, {'from': acc})
+
+    # Cannot mint an index that has
+    # not yet been released
+    with brownie.reverts():
+        dc.mintDate(2**200, {'from': acc})  
+
+    # Cannot mint tomorrow's index
+    with brownie.reverts():
+        dc.mintDate(today_idx + 1, {'from': acc})   
+
+    # But will work after enough time
+    # has passed
+    chain.sleep(24 * 60 * 60)
+    dc.mintDate(today_idx + 1, {'from': acc})
+
+    # We can also wait for the exact
+    # amount of time before minting
+    seconds_passed = chain[-1].timestamp % (24 * 60 * 60)
+    chain.sleep((24 * 60 * 60) - seconds_passed - 1)
+    # Wont work now
+    with brownie.reverts():
+        dc.mintDate(today_idx + 2, {'from': acc})
+    # But will work if we wait 1 second
+    chain.sleep(1)
+    dc.mintDate(today_idx + 2, {'from': acc})
+
+
+def test_perpetual_mint(dc, accounts, chain):
+    acc = accounts[1]
+    today_idx = dc.blockDateIndex()
+
+    for i in range(10):
+        today_idx += 1
+        # Cannot mint in the future
+        with brownie.reverts():
+            dc.mintDate(today_idx, {'from': acc})   
+        # But can after enough time has passed
+        chain.sleep(24 * 60 * 60)
+        dc.mintDate(today_idx, {'from': acc}) 
+
+
