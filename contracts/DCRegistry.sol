1cd7f6a9ca43cec8d0093a66a451779374ef974d
dcgub <holdomjeremie@gmail.com>
2022-11-09 18:54:30 -0500

    zz

--- contracts/DCRegistry.sol
+++ contracts/DCRegistry.sol
@@ -1,13 +1,23 @@
 contract DCRegistry {
 
-	DateCalendar dc;
+	DateCalendar public dc;
 
 	struct Record {
         address resolver;
-        uint64 ttl;
+        uint256 expiry;
 	}
 
+    // One record for each date token index
 	mapping(uint256 => Record) private _records;
+
+    // Only controllers can set a record
+    // Controller can be changed
+
+    // On a controller
+    // Only owner of a date can set/change resolver if record is still active.
+    // Only owner of date can extend expiry
+
+    // How to add registrar layer that can change pricing etc?
 
     modifier is_approved_or_owner(uint256 dateTokenIndex) {
         address owner = dc.ownerOf(dateTokenIndex);
@@ -15,7 +25,8 @@ contract DCRegistry {
         _;
     }
 
-	function setRecord(uint256 dateTokenIndex) external is_approved_or_owner(dateTokenIndex);
+    // Only the owner or an approved operator of the date can set the record...but cannot set ttl
+	function setRecord(uint256 dateTokenIndex, address resolver, uint64 ttl) external is_approved_or_owner(dateTokenIndex);
 
     function resolver(uint256 dateTokenIndex) external view returns (address);
 
